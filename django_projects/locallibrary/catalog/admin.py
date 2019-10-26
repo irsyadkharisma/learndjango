82c2f1aafb7b9c83d757726894e8ebe095a628e0
irsyadkharisma <irsyadkh@gmail.com>
2019-10-26 22:17:27 +0700

    django admin moved

--- django_projects/locallibrary/catalog/admin.py
+++ django_projects/locallibrary/catalog/admin.py
@@ -5,7 +5,15 @@ from django.contrib import admin
 from catalog.models import Author, Genre, Book, BookInstance, Language
 
 admin.site.register(Book)
-admin.site.register(Author)
+#admin.site.register(Author)
 admin.site.register(Genre)
 admin.site.register(BookInstance)
 admin.site.register(Language)
+
+
+#admin register baru
+
+class AuthorAdmin(admin.ModelAdmin):
+    pass
+
+admin.site.register(Author, AuthorAdmin)
